0f0cff315e55286b06b43bd5398ee15b111e4c80
Vitor Justamand <justamandd@gmail.com>
2024-05-28 00:44:39 -0300

    feat(recipe): recipe card improved

--- src/components/RecipeCard.tsx
+++ src/components/RecipeCard.tsx
@@ -29,10 +29,10 @@ const RecipeCard: React.FC<RecipeCardProps> = ({ recipeProps }) => {
         aria-labelledby="modal-modal-title"
         aria-describedby="modal-modal-description"
       >
-        <div className="w-[70vw] h-[90vh] bg-white rounded-[4px] py-[20px] px-[40px]">
+        <div className="min-w-[30vw] min-h-[30vh] bg-white rounded-[4px] py-[20px] px-[40px]">
           <div className="flex justify-between items-center mb-[40px]">
             <h1 className="text-md text-title font-semibold ">
-              {recipeProps.name}
+              {recipeProps.name == "" ? (<span>Nome não adicionado</span>) : (<span>{recipeProps.name}</span>)}
             </h1>
             <AiOutlineClose
               size={25}
@@ -42,25 +42,43 @@ const RecipeCard: React.FC<RecipeCardProps> = ({ recipeProps }) => {
           </div>
           <div className="mb-6">
             <h2 className="font-semibold mb-2">Ingredientes</h2>
-            <ul className="ml-5 list-disc">
-              {recipeProps.ingredients.map((ingredient: string) => (
-                <li>{ingredient.charAt(0).toUpperCase() + ingredient.slice(1)}</li>
-              ))}
-            </ul>
+            {recipeProps.ingredients.length!=0 ? (
+              <ul className="ml-5 list-disc">
+                {recipeProps.ingredients.map((ingredient: string) => (
+                  <li>{ingredient.charAt(0).toUpperCase() + ingredient.slice(1)}</li>
+                ))}
+              </ul>
+            ):(
+              <p>Nenhum ingrediente especificado</p>
+            )}
+            
           </div>
           <div className="mb-6">
             <h2 className="font-semibold mb-2">Preparação</h2>
-            <ul className="ml-5 list-decimal">
-              {
-              recipeProps.preparationMethod.map((prep: string) => (
-                <li>{prep.charAt(0).toUpperCase() + prep.slice(1)}</li>
-              ))}
-            </ul>
+            {recipeProps.preparationMethod.length!=0 ? (
+              <ul className="ml-5 list-decimal">
+                {
+                recipeProps.preparationMethod.map((prep: string) => (
+                  <li>{prep.charAt(0).toUpperCase() + prep.slice(1)}</li>
+                ))}
+              </ul>
+            ):(
+              <p>Nenhuma preparação adicionada</p>
+            )}
           </div>
-          <div className="mb-6">
+          <div className="mb-[40px]">
             <h2 className="font-semibold mb-2">Tempo de preparo</h2>
-            <p className="flex items-center"><AiOutlineClockCircle color="#667085" size={20}></AiOutlineClockCircle>&nbsp;{recipeProps.preparationTime} minutos</p>
+            <p className="flex items-center"><AiOutlineClockCircle color="#667085" size={20}></AiOutlineClockCircle>&nbsp;{
+              recipeProps.preparationTime == 0 ? 
+                (
+                  <span>Tempo não especificado</span>
+                ):
+                (
+                  <span>{recipeProps.preparationTime} minutos</span>
+                )
+            }</p>
           </div>
+          <Button title="Fechar" backgroundColor="bg-gray-500" onClick={closeModal}></Button>
         </div>
       </Modal>
       <div className="w-full h-[50px] bg-white flex items-center justify-between px-[10px] my-[10px] rounded-[4px] shadow-sm">
