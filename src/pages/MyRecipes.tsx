bd39bcf714f969e955bb8e7577260ddc8d587c4c
Gabriel Hervatin <Gabriel.Hervatin@br.nestle.com>
2024-10-01 08:49:24 -0300

    Lista de compras

--- src/pages/MyRecipes.tsx
+++ src/pages/MyRecipes.tsx
@@ -11,26 +11,38 @@ import { IoIosCloseCircleOutline } from "react-icons/io";
 import { IoSearchOutline } from "react-icons/io5";
 import { RecipeProps } from "../interfaces/RecipeProps";
 import Swal from "sweetalert2";
-import { IngredientRecipeDTO } from "../interfaces/RecipeIADTO";
+import FlatList from "flatlist-react";
+
+interface Ingredient {
+  _id: string;
+  Descrip: string;
+}
 
 const MyRecipes: React.FC = () => {
   const [openModal, setOpenModal] = useState(false);
   const [filterText, setFilterText] = useState("");
+  const [ingredientSearchText, setIngredientSearchText] = useState("");
   const [visible, setVisible] = useState(false);
   const [recipes, setRecipes] = useState<RecipeProps[]>([]);
   const [newRecipeName, setNewRecipeName] = useState("");
-  const [ingredientsList, setIngredientsList] = useState<string[]>([]);
-  const [newIngredient, setNewIngredient] = useState("");
+  const [ingredientsList, setIngredientsList] = useState<Ingredient[]>([]);
   const [ingredientGrams, setIngredientGrams] = useState<{ [key: string]: string }>({});
   const [newPreparationStep, setNewPreparationStep] = useState("");
   const [preparationMethodList, setPreparationMethodList] = useState<string[]>([]);
   const [newPreparationTime, setNewPreparationTime] = useState(0);
   const [showError, setShowError] = useState(false);
+  const [allIngredients, setAllIngredients] = useState<Ingredient[]>([]);
   const { getToken } = useAuth();
 
   useEffect(() => {
     fetchRecipes();
   }, []);
+
+  useEffect(() => {
+    if (openModal) {
+      fetchAllIngredients();
+    }
+  }, [openModal]);
 
   const fetchRecipes = async () => {
     try {
@@ -44,6 +56,21 @@ const MyRecipes: React.FC = () => {
       }
     } catch (error) {
       console.error(error);
+    }
+  };
+
+  const fetchAllIngredients = async () => {
+    try {
+      const token = await getToken();
+      const response = await api.get("/api/v1/ingredients", {
+        headers: { Authorization: `Bearer ${token}` },
+      });
+
+      if (response.data) {
+        setAllIngredients(response.data.data);
+      }
+    } catch (error) {
+      console.error("Error fetching ingredients:", error);
     }
   };
 
@@ -63,7 +90,7 @@ const MyRecipes: React.FC = () => {
     try {
       const token = await getToken();
       const formData = getFormData();
-      console.log(formData)
+      console.log(formData);
 
       const response = await api.post("/api/v1/user/recipe", formData, {
         headers: {
@@ -100,7 +127,7 @@ const MyRecipes: React.FC = () => {
   const handleDeleteIngredient = (index: number) => {
     const ingredientToDelete = ingredientsList[index];
     setIngredientsList(ingredientsList.filter((_, i) => i !== index));
-    const { [ingredientToDelete]: _, ...rest } = ingredientGrams;
+    const { [ingredientToDelete.Descrip]: _, ...rest } = ingredientGrams;
     setIngredientGrams(rest);
   };
 
@@ -121,11 +148,10 @@ const MyRecipes: React.FC = () => {
     setOpenModal(false);
   };
 
-  const addIngredientToList = () => {
-    if (newIngredient.trim() === "") return;
-    setIngredientsList([...ingredientsList, newIngredient]);
-    setIngredientGrams({ ...ingredientGrams, [newIngredient]: '' });
-    setNewIngredient('');
+  const addIngredientToList = (ingredient: Ingredient) => {
+    if (ingredientsList.some((ing) => ing._id === ingredient._id)) return;
+    setIngredientsList([...ingredientsList, ingredient]);
+    setIngredientGrams({ ...ingredientGrams, [ingredient.Descrip]: '' });
   };
 
   const addPreparationStepToList = () => {
@@ -138,7 +164,6 @@ const MyRecipes: React.FC = () => {
   const resetRecipeItemsInputs = () => {
     setIngredientsList([]);
     setPreparationMethodList([]);
-    setNewIngredient("");
     setNewPreparationStep("");
     setShowError(false);
   };
@@ -154,8 +179,8 @@ const MyRecipes: React.FC = () => {
     return {
       name: newRecipeName,
       ingredients: ingredientsList.map((ingredient) => ({
-        name: ingredient,
-        quantity: ingredientGrams[ingredient],
+        name: ingredient.Descrip,
+        quantity: ingredientGrams[ingredient.Descrip],
       })),
       preparationMethod: preparationMethodList,
       preparationTime: newPreparationTime,
@@ -171,14 +196,17 @@ const MyRecipes: React.FC = () => {
     );
   };
 
+  const filteredIngredients = allIngredients.filter((ingredient) =>
+    ingredient.Descrip.toLowerCase().includes(ingredientSearchText.toLowerCase())
+  );
+
   return (
     <>
       <Modal
         style={{
+          display: "flex",
           alignItems: "center",
           justifyContent: "center",
-          alignSelf: "center",
-          justifySelf: "center",
         }}
         open={openModal}
         aria-labelledby="modal-modal-title"
@@ -212,35 +240,41 @@ const MyRecipes: React.FC = () => {
               Ingredientes
             </h2>
             <div className="mb-4 flex">
-              <input
-                type="text"
-                value={newIngredient}
-                onKeyDown={(e) => {
-                  if (e.key === "Enter") {
-                    e.preventDefault();
-                    addIngredientToList();
-                  }
-                }}
-                onChange={(e) => setNewIngredient(e.target.value)}
-                className="shadow appearance-none border rounded w-4/5 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                placeholder="Novo Ingrediente"
+              <Input
+                value={ingredientSearchText}
+                onChange={(e) => setIngredientSearchText(e.target.value)}
+                placeholder="Buscar Ingrediente"
+                firstIcon={<IoSearchOutline color="#667085" size={20} />}
+                icon={
+                  <button onClick={() => setIngredientSearchText("")}>
+                    <IoIosCloseCircleOutline color="#667085" size={20} />
+                  </button>
+                }
               />
-              <Button
-                type="button"
-                disabled={newIngredient.trim() === ""}
-                title="Adicionar"
-                width="w-1/5"
-                onClick={addIngredientToList}
+            </div>
+            <div className="mb-4 h-[150px] overflow-y-scroll border p-2 rounded">
+              <FlatList
+                list={filteredIngredients}
+                renderItem={(ingredient, index) => (
+                  <div
+                    key={index}
+                    className="flex justify-between items-center cursor-pointer hover:bg-gray-200 p-2 rounded"
+                    onClick={() => addIngredientToList(ingredient)}
+                  >
+                    <span>{ingredient.Descrip}</span>
+                  </div>
+                )}
+                renderWhenEmpty={() => <div className="text-center">Nenhum ingrediente encontrado</div>}
               />
             </div>
             <ul className="ml-5 list-disc mb-4">
               {ingredientsList.map((ingredient, index) => (
                 <li key={index} className="flex items-center">
-                  {index + 1}. {ingredient}
+                  {index + 1}. {ingredient.Descrip}
                   <input
                     type="number"
-                    value={ingredientGrams[ingredient] || ""}
-                    onChange={(e) => handleGramsChange(ingredient, e.target.value)}
+                    value={ingredientGrams[ingredient.Descrip] || ""}
+                    onChange={(e) => handleGramsChange(ingredient.Descrip, e.target.value)}
                     className="ml-2 border rounded w-20 px-2 py-1 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                     placeholder="Gramas"
                   />
@@ -362,7 +396,6 @@ const MyRecipes: React.FC = () => {
                   key={recipe.id}
                   recipeProps={recipe}
                   fetchRecipes={fetchRecipes}
-                  
                 />
               ))}
             </div>
